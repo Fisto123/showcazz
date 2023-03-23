7bf72238f7b81826dec177928087cce1b75da450
Fisto123 <122396073+Fisto123@users.noreply.github.com>
2023-03-23 14:58:11 +0100

    Update newRequest.js

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const newRequest = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://showcazzz.onrender.com/",
   withCredentials: true,
 });

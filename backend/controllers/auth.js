6765fd33a31bd105892192dcb14378d44d2739cb
Fisto123 <122396073+Fisto123@users.noreply.github.com>
2023-03-23 17:23:02 +0100

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -44,6 +44,8 @@ export const login = async (req, res, next) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
+        secure:true,
+      sameSite:"none"
       })
       .status(200)
       .send(info);
